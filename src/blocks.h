4a342855993270859f4582767055b4029fc93c54
Alberto G. Jácome <agjacome@gmail.com>
2020-04-13 17:59:47 +0200

    reduce interval for update counter

--- src/blocks.h
+++ src/blocks.h
@@ -2,7 +2,7 @@ static const Block blocks[] = {
     /* Icon         Command               Interval    Signal */
     { "\uF001 ",    "dwmblocks-music",    5,          0      },
     { "\uF0E0 ",    "dwmblocks-email",    5,          0      },
-    { "\uF187 ",    "dwmblocks-updates",  3600,       0      },
+    { "\uF187 ",    "dwmblocks-updates",  600,        0      },
     { "\uE266 ",    "dwmblocks-cpu",      15,         0      },
     { "\uF1C0 ",    "dwmblocks-memory",   15,         0      },
     { "\uF028 ",    "dwmblocks-volume",   5,          0      },
