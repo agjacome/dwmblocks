bfe109b0ba4d1a4f9eeae986a8594b5ebe28ce5f
Alberto G. Jácome <contact@agjacome.dev>
2024-09-27 23:33:36 +0200

    disable unread email count

--- src/blocks.h
+++ src/blocks.h
@@ -1,7 +1,7 @@
 static const Block blocks[] = {
     /* Icon, Command, Interval, Signal */
     { "\uF001  ", "/opt/dwmblocks/music",    5,   0 },
-    { "\uF0E0  ", "/opt/dwmblocks/email",    5,   0 },
+//  { "\uF0E0  ", "/opt/dwmblocks/email",    5,   0 },
     { "\uF187  ", "/opt/dwmblocks/updates",  600, 0 },
     { "\uE266  ", "/opt/dwmblocks/cpu",      15,  0 },
     { "\uF1C0  ", "/opt/dwmblocks/memory",   15,  0 },
